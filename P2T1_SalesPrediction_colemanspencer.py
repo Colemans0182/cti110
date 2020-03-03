c7cd4d579e296b21c900ad456e2ec7ab07adaa74
Colemans0182 <61761720+Colemans0182@users.noreply.github.com>
2020-03-03 17:44:08 -0500

    First submission
    Spencer coleman jr

---
+++ P2T1_SalesPrediction_colemanspencer.py
@@ -0,0 +1,16 @@
+#Display the name of student
+print('Spencer Coleman')
+#Display the calss
+print('CTI-110 P2T1_Sales Prediction')
+#display the date
+print('February 20, 2020')
+
+
+#Get the projected total sales.
+total_sales =float(input('Enter the porjected sales: '))
+
+#calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+#Display the profit
+print('The profit is $', format(profit, ',.2f'))
